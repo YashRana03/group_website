2a0cfa7d123c02279fc528997f3d9203f61a9758
Yash rana <97224327+YashRana03@users.noreply.github.com>
2024-02-22 21:05:52 +0000

    Add image slideshow

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
 const arrowEl = document.getElementsByClassName("arrow-icon")[0];
 
+const infoIcons = document.getElementsByClassName("info-logo");
+
 setInterval(() => {
-  arrowEl.style.transform = "translateY(-20px)";
+  arrowEl.style.transform = "translateY(-10px)";
   setTimeout(() => {
-    arrowEl.style.transform = "translateY(20px)";
+    arrowEl.style.transform = "translateY(10px)";
   }, 1500);
 }, 3000);
 
 function moveArrow() {
-  arrowEl.style.transform = "translateY(-20px)";
+  arrowEl.style.transform = "translateY(-10px)";
   setTimeout(() => {
-    arrowEl.style.transform = "translateY(20px)";
+    arrowEl.style.transform = "translateY(10px)";
   }, 1500);
 }
 
